fe63b47f4bd2faa2266c569362f3e002c81e25e9
Alex <81256412+zorn7680@users.noreply.github.com>
2022-11-12 20:03:57 +0100

    Extended class to handle control byte creation

---
+++ ProPra_Alex/src/propra/imageconverter/compression/ControlByte.java
@@ -0,0 +1,48 @@
+package propra.imageconverter.compression;
+
+/* This class represents a control byte for compression/decompression. */
+
+public class ControlByte {
+	final byte STEUERBIT = (byte) 0b10000000;
+	final int UNSIGNED_MASK = 0xFF;
+
+	private int type;
+	private int length;
+	private byte inByte;
+
+	public ControlByte(byte inByte) {
+		this.inByte = inByte;
+		this.type = (STEUERBIT & inByte) != 0 ? 1 : 0;
+		this.length = (~STEUERBIT & inByte) + 1;
+	}
+
+	public ControlByte(int length, int type) {
+		this.length = length;
+		this.type = type;
+	}
+
+//	public void printInByte() {
+//		System.out.println(inByte);
+//	}
+
+
+
+	public byte generateControlByte(boolean wh, int repCounter) {
+		int result;
+		if (wh) {
+			result = repCounter;
+		} else {
+			result = repCounter + 128;
+		}
+
+		return (byte) result;
+	}
+
+	public int getType() {
+		return this.type;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
+}
