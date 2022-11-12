18f400554e31169cf3e7bf398fb64db229a36f35
Alex <81256412+zorn7680@users.noreply.github.com>
2022-11-12 11:47:46 +0100

    class BaseTools for Base32 encoding added

---
+++ ProPra_Alex/src/propra/imageconverter/tools/BaseTools.java
@@ -0,0 +1,24 @@
+package propra.imageconverter.tools;
+
+public class BaseTools {
+	
+	public static void base32Encode() {
+		
+	}
+	
+	public static void base32Decode() {
+		
+	}
+	
+	
+	
+	// OPTIONAL
+	public static void baseNEncode() {
+		
+	}
+	
+	public static void baseNDecode() {
+		
+	}
+
+}
