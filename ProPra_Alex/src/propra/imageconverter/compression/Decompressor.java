31044d9d57196b19990dbab37c0b3f44e62db683
Alex <81256412+zorn7680@users.noreply.github.com>
2022-11-17 15:40:23 +0100

    better commenting added

--- ProPra_Alex/src/propra/imageconverter/compression/Decompressor.java
+++ ProPra_Alex/src/propra/imageconverter/compression/Decompressor.java
@@ -1,27 +1,31 @@
 package propra.imageconverter.compression;
 
+/*
+ * CHANGELOG Abschnitt 2
+ * - Klasse neu eingeführt, keine Vorgängerversion vorhanden
+ */
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
+/*
+ * Diese Klasse dekomprimiert RGB-Daten, die als Byte-Array übergeben werden,
+ * gemäß der Spezifikation zur RLE-Kompression.  Die dekomprimierte RGB-
+ * Reihenfolge bleibt unverändert zur komprimierten.
+ */
 
 public class Decompressor {
 
 	private ByteArrayInputStream inStream;
 	private ByteArrayOutputStream outStream;
 
-	/**
-	 * Method decompresses given byte array and returns byte array with decompressed
-	 * RGB information. Decompressed pixel byte order is equal to compressed pixel
-	 * byte order (TGA format: BGR, ProPra format: RBG).
-	 * 
-	 * @param inputData byte array of compressed RGB information with control bytes
-	 * @return byte array with uncompressed RGB information
-	 * @throws IOException
-	 */
+
 	public byte[] decompress(byte[] inputData) throws IOException {
 		System.out.println("\nCompressionTools.decompress() invoked!\n");
 		byte[] result;
 		byte[] rgbSequence;
+
 
 		inStream = new ByteArrayInputStream(inputData);
 		outStream = new ByteArrayOutputStream();
@@ -32,20 +36,16 @@ public class Decompressor {
 
 		while (inStream.available() > 0) {
 			cByte = new ControlByte(inStream.readNBytes(1)[0]);
+
 			cbType = cByte.getType();
 			cbLength = cByte.getLength();
 
-			System.out.println("cb Typ: " + cbType);
-			System.out.println("cb Länge: " + cbLength);
-
-			/* control byte type is 0 - sequence with individual pixels */
 			if (cbType == 0) {
-				System.out.println("Sequenz!\n");
+				// Es folgt eine RAW-Paket
 				rgbSequence = inStream.readNBytes(cbLength * 3);
 				outStream.write(rgbSequence);
-				/* control byte type is 1 - block with repeating pixels */
 			} else {
-				System.out.println("Wiederholung!\n");
+				// Es folgt ein RunLength-Paket
 				rgbSequence = inStream.readNBytes(3);
 				for (int i = 1; i <= cbLength; i++) {
 					outStream.write(rgbSequence);
@@ -59,5 +59,4 @@ public class Decompressor {
 
 		return result;
 	}
-
 }
